8bd018893723ad79cb282f9795549baa682e3c5e
WDanielH <warrendh@gmail.com>
2020-10-17 15:58:48 -0400

    added complete button

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,46 +3,58 @@
  import React, { useState } from 'react';
  import './todo.css';
 
- function Task({ task }) {
-     return (
-         <div
-             className="task"
-             style={{ textDecoration: task.completed ? "line-through" : "" }}
-         >
-             {task.title}
-         </div>
-     );
- }
+ function Task({ task, index, completeTask }) {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+            <button onClick={() => completeTask(index)}>Complete</button>
+        </div>
+    );
+}
+
  function Todo() {
-     const [tasks, setTasks] = useState([
-         {
-             title: "Grab some Pizza",
-             completed: true
-         },
-         {
-             title: "Do your workout",
-             completed: true
-         },
-         {
-             title: "Hangout with friends",
-             completed: false
-         }
-     ]);
-     return (
-         <div className="todo-container">
-             <div className="header">TODO - ITEMS</div>
-             <div className="tasks">
-                 {tasks.map((task, index) => (
-                     <Task
-                         task={task}
-                         index={index}
-                         key={index}
-                     />
-                 ))}
-             </div>
-         </div>
-     );
- }
+    const [tasks, setTasks] = useState([
+        {
+            title: "Grab some Pizza",
+            completed: true
+        },
+        {
+            title: "Do your workout",
+            completed: true
+        },
+        {
+            title: "Hangout with friends",
+            completed: false
+        }
+    ]);
+
+    const addTask = title => {
+        const newTasks = [...tasks, { title, completed: false }];
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-container">
+            <div className="header">TODO - ITEMS</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                        task={task}
+                        index={index}
+                        key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
+
 
 function CreateTask({addTask}){
     const [value, setValue]= useState("");
