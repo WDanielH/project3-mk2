9506679d6dc837d81a25dafbaaf406a62d047d7f
WDanielH <warrendh@gmail.com>
2020-10-17 15:51:18 -0400

    basic static design implemented

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,48 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import { render } from "react-dom" 
-import { createStore } from 'redux'
-import { Provider, connect } from 'react-redux'
+ // Todo.js
 
-function Task({ task }) {
-    return (
-        <div
-            className="task"
-            style={{ textDecoration: task.completed ? "line-through" : "" }}
-        >
-            {task.title}
-        </div>
-    );
-}
-function Todo() {
-    const [tasks, setTasks] = useState([
-        {
-            title: "Grab some Pizza",
-            completed: true
-        },
-        {
-            title: "Do your workout",
-            completed: true
-        },
-        {
-            title: "Hangout with friends",
-            completed: false
-        }
-    ]);
-    return (
-        <div className="todo-container">
-            <div className="header">TODO - ITEMS</div>
-            <div className="tasks">
-                {tasks.map((task, index) => (
-                    <Task
-                        task={task}
-                        index={index}
-                        key={index}
-                    />
-                ))}
-            </div>
-        </div>
-    );
-}
+ import React, { useState } from 'react';
+ import './todo.css';
 
-export default Todo;
+ function Task({ task }) {
+     return (
+         <div
+             className="task"
+             style={{ textDecoration: task.completed ? "line-through" : "" }}
+         >
+             {task.title}
+         </div>
+     );
+ }
+ function Todo() {
+     const [tasks, setTasks] = useState([
+         {
+             title: "Grab some Pizza",
+             completed: true
+         },
+         {
+             title: "Do your workout",
+             completed: true
+         },
+         {
+             title: "Hangout with friends",
+             completed: false
+         }
+     ]);
+     return (
+         <div className="todo-container">
+             <div className="header">TODO - ITEMS</div>
+             <div className="tasks">
+                 {tasks.map((task, index) => (
+                     <Task
+                         task={task}
+                         index={index}
+                         key={index}
+                     />
+                 ))}
+             </div>
+         </div>
+     );
+ }
+
+ export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Todo} from './components/todo';
+import Todo from "./components/todo";
 import * as serviceWorker from './serviceWorker';
 
   ReactDOM.render(<Todo />, document.getElementById('root'));
