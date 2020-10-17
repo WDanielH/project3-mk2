9fc1a58181b3d3f426e5d53613d912204b6a7186
WDanielH <warrendh@gmail.com>
2020-10-17 16:05:58 -0400

    can now add tasks

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,15 +3,38 @@
  import React, { useState } from 'react';
  import './todo.css';
 
- function Task({ task, index, completeTask }) {
+ function Task({ task }) {
+     return (
+         <div
+             className="task"
+             style={{ textDecoration: task.completed ? "line-through" : "" }}
+         >
+             {task.title}
+         </div>
+     );
+ }
+
+ function CreateTask({ addTask }) {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+
+        addTask(value);
+        setValue("");
+    }
+
     return (
-        <div
-            className="task"
-            style={{ textDecoration: task.completed ? "line-through" : "" }}
-        >
-            {task.title}
-            <button onClick={() => completeTask(index)}>Complete</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add a new task"
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
     );
 }
 
@@ -36,22 +59,15 @@ function Todo() {
         setTasks(newTasks);
     };
 
-    const completeTask = index => {
-        const newTasks = [...tasks];
-        newTasks[index].completed = true;
-        setTasks(newTasks);
-    };
-
     return (
         <div className="todo-container">
             <div className="header">TODO - ITEMS</div>
             <div className="tasks">
                 {tasks.map((task, index) => (
                     <Task
-                    task={task}
-                    index={index}
-                    completeTask={completeTask}
-                    key={index}
+                        task={task}
+                        index={index}
+                        key={index}
                     />
                 ))}
             </div>
@@ -59,30 +75,6 @@ function Todo() {
                 <CreateTask addTask={addTask} />
             </div>
         </div>
-    );
-}
-
-
-function CreateTask({addTask}){
-    const [value, setValue]= useState("");
-
-    const handleSubmit = e =>{
-        e.preventDefault();
-        if (!value)return;
-
-        addTask(Value);
-        setValue("");
-    }
-    return(
-        <form onSubmit={handleSubmit}>
-        <input
-            type="text"
-            className="input"
-            value={value}
-            placeholder="Add a new task"
-            onChange={e => setValue(e.target.value)}
-        />
-    </form>   
     );
 }
 
