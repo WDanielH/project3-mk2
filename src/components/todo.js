4aef527a9d853bdbfc0ab1a77d3d27ce962cc1d1
WDanielH <warrendh@gmail.com>
2020-10-17 15:34:33 -0400

    import issues

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,3 +3,46 @@ import ReactDOM from 'react-dom';
 import { render } from "react-dom" 
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
+
+function Task({ task }) {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+        </div>
+    );
+}
+function Todo() {
+    const [tasks, setTasks] = useState([
+        {
+            title: "Grab some Pizza",
+            completed: true
+        },
+        {
+            title: "Do your workout",
+            completed: true
+        },
+        {
+            title: "Hangout with friends",
+            completed: false
+        }
+    ]);
+    return (
+        <div className="todo-container">
+            <div className="header">TODO - ITEMS</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                        task={task}
+                        index={index}
+                        key={index}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
