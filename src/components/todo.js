03174253fd45fd82cae101297799eb0316235cfb
WDanielH <warrendh@gmail.com>
2020-10-17 15:54:57 -0400

    added create task

--- src/components/todo.js
+++ src/components/todo.js
@@ -44,4 +44,27 @@
      );
  }
 
+function CreateTask({addTask}){
+    const [value, setValue]= useState("");
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        if (!value)return;
+
+        addTask(Value);
+        setValue("");
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add a new task"
+            onChange={e => setValue(e.target.value)}
+        />
+    </form>   
+    );
+}
+
  export default Todo;
