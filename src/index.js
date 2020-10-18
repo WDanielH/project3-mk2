523b25455e883c670a6d4d62a924ac29e532a814
WDanielH <warrendh@gmail.com>
2020-10-17 20:42:54 -0400

    it will now load

--- src/index.js
+++ src/index.js
@@ -12,16 +12,21 @@ import todoApp from './reducers/reducers';
 let store = createStore(todoApp);
 
 let rootElement = document.getElementById('root');
-React.render(
+ReactDOM.render(
   // The child must be wrapped in a function
   // to work around an issue in React 0.13.
   <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+/*
+ <Provider store={store}>
     {() => <App />}
   </Provider>,
   rootElement
-);
-
-
+*/
 
 
    
