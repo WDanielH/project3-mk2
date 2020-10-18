55aee2ea1771e70aec51f47bf166d92055098736
WDanielH <warrendh@gmail.com>
2020-10-18 01:36:42 -0400

    cleared out old commented out code

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -45,33 +45,3 @@ const mapDispatchToProps = dispatch => ({
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
 
 
-
-/*
-
-import React, { Component} from 'react';
-import PropTypes from 'prop-types';
-import Todo from '../components/Todo';
-
-export default class TodoList extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo, index) =>
-          <Todo {...todo}
-                key={index}
-                onClick={() => this.props.onTodoClick(index)} />
-        )}
-      </ul>
-    );
-  }
-}
-
-TodoList.propTypes = {
-  onTodoClick: PropTypes.func.isRequired,
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
-  }).isRequired).isRequired
-};
-
-*/
