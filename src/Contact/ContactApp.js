7b0ab1d4a87690626043a1bb87f92e134976390a
WDanielH <warrendh@gmail.com>
2020-10-18 16:45:11 -0400

    skeleton of forms complete

--- src/Contact/ContactApp.js
+++ src/Contact/ContactApp.js
@@ -1,33 +1,49 @@
 
-// import react
+import React, { Component } from "react";
+
 
 // export the default function "app"
-// set our fields as consts 
+export default function App(){
+    const [FirstName, setFirstName] = React.useState("");
+    const [LastName, setLastName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [Comments, setComments] = React.useState("");
 
-// do a handle submit
+    const handleSubmit = (event) => {
+        console.log(`
+        FirstName: ${firstname}
+        LastName: ${lastname}
+          Email: ${email}
+          Comments:  ${comments}
+        `);
+        
+        event.preventDefault();
+    }
+    
+return (
+    <form onSubmit={handleSubmit}>
+        <h1>Contact Us</h1>
 
-//form
-//labels and input - several 
+        <label>
+            First Name:    
+        </label>
 
-<form onSubmit={handleSubmit}>
-<h1>Contact Us</h1>
+        <label>
+            Last Name:
+        </label>
 
-<label>
-    First Name:    
-</label>
+        <label>
+            Email:
+        </label>
 
-<label>
-    Last Name:
-</label>
+        <label>
+            Comments:
+        </label>
 
-<label>
-    Email:
-</label>
+        <button>Submit</button>
 
-<label>
-    Comments:
-</label>
+    </form>
+);
+}
 
-<button>Submit</button>
 
-</form>
