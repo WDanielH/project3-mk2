270518fcdc1d5e576e2e25d7d0166835157dbcbb
WDanielH <warrendh@gmail.com>
2020-10-17 21:31:54 -0400

    tossed old reference as it was depreciated

--- src/actions/todo.actions.js
+++ src/actions/todo.actions.js
@@ -1,6 +1,34 @@
+
+let nextTodoId = 0;
+
+export const addTodo = text => ({
+  type: "ADD_TODO",
+  id: nextTodoId++,
+  complete: false,
+  text
+});
+
+export const toggleTodo = id => ({
+  type: "TOGGLE_TODO",
+  id
+});
+
+export const filter = filter => ({
+  type: "FILTER_TODO",
+  filter
+});
+
+export const typeFilter = {
+  SHOW_COMPLETE: "SHOW_COMPLETE",
+  SHOW_ACTIVE: "SHOW_ACTIVE",
+  SHOW_ALL: "SHOW_ALL"
+};
+
+
+
 /*
- * action types
- */
+//action types
+
 
 export const CREATE_TASK = 'CREATE_TASK';
 export const COMPLETE_TASK = 'COMPLETE_TASK';
@@ -11,9 +39,9 @@ export const REMOVE_TASK = 'REMOVE_TASK';
 // This one isn’t in the react example
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
-/*
- * other constants
- */
+
+//other constants
+
 
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
@@ -21,9 +49,9 @@ export const VisibilityFilters = {
   SHOW_ACTIVE: 'SHOW_ACTIVE'
 };
 
-/*
- * action creators
- */
+
+//action creators
+
 
 export function createTask(text) {
   return { type: CREATE_TASK, text };
@@ -40,3 +68,4 @@ export function removeTask(index) {
 export function setVisibilityFilter(filter) {
   return { type: SET_VISIBILITY_FILTER, filter };
 }
+*/
