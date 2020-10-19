07fb1121106778262d0f3e5152696804d88e306a
WDanielH <warrendh@gmail.com>
2020-10-18 22:55:45 -0400

    basic navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import AddTodo from "../containers/AddTodo";
 import TodoList from "../containers/TodoList";
 import Footer from "../containers/Footer";
+import Navbar from "../containers/Navbar";
 import "./Todo.css";
 
 
 const App = () => (
     <div classname="todo-container">
-      
+    <Navbar /> 
     <AddTodo />
     <TodoList />
     <Footer />
---
+++ src/containers/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "../components/Link";
+import '../components/Todo.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a className="navlink">ToDo List</a>
+      <a className="navlink">Contact Us</a>
+    </div>
+  );
+};
+
+export default Navbar;
+
